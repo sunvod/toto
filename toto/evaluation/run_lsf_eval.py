203e40a115e0e0901d1964680d49a447970f4e5e
Anna-Monica Toon <annamonica.toon@datadoghq.com>
2025-05-13 18:07:42 +0200

    add copyright headers

--- toto/evaluation/run_lsf_eval.py
+++ toto/evaluation/run_lsf_eval.py
@@ -1,3 +1,8 @@
+# Unless explicitly stated otherwise all files in this repository are licensed under the Apache-2.0 License.
+#
+# This product includes software developed at Datadog (https://www.datadoghq.com/)
+# Copyright 2025 Datadog, Inc.
+
 """
 This script evaluates a TOTO model on LSF datasets using a specified checkpoint dir path or model id.
 It supports evaluating multiple datasets, prediction lengths, and context lengths in parallel using Ray.
